629994e37fbb14f585425630c1d8c146c195bb23
Gsiniguez <gonza.ini94@gmail.com>
2020-07-02 14:32:08 -0300

    fix config store

--- src/Store/ConfigureStore.js
+++ src/Store/ConfigureStore.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import rootReducer from '../Reducers/Index'
+import rootReducer from '../Reducers/index'
 
 export default initialState => {
     return createStore(rootReducer, initialState, applyMiddleware(thunk))
