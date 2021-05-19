24ad064b95ddffcac6f7f19c444d4d8f446be764
Gonzalo Iñiguez <31194342+Gsiniguez@users.noreply.github.com>
2021-05-19 16:45:24 -0300

    devtools redux

--- src/Store/ConfigureStore.js
+++ src/Store/ConfigureStore.js
@@ -1,7 +1,12 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from '../Reducers/index'
+import { composeWithDevTools } from "redux-devtools-extension";
+
+
+const middlewareEnhancer = applyMiddleware(thunk)
+const composedEnchancers = compose(middlewareEnhancer, composeWithDevTools())
 
 export default initialState => {
-    return createStore(rootReducer, initialState, applyMiddleware(thunk))
+    return createStore(rootReducer, initialState, composedEnchancers)
 }
